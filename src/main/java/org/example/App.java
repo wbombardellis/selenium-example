96326d4b328e80219d443b63ba122000b094af29
William Bombardelli da Silva <wbombardellis@gmail.com>
2022-02-05 11:38:34 -0600

    Finish example

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -43,18 +43,32 @@ public class App
 
             loginButton.click();
 
-            By errorMessageLocator = By.xpath("//*[contains(text(),‘Invalid Login’)]");
-            var invalidLoginMsg = new WebDriverWait(driver, Duration.ofSeconds(10))
-                    .until(d -> d.findElement(errorMessageLocator));
+            System.out.println(driver.getCurrentUrl());
+            if (driver.getCurrentUrl().matches("https\\://account.blip.ai/login.*"))
+                System.out.println("Success");
+            else
+                System.out.println("Fail");
 
-            System.out.println("done");
 
-            /// Second scenario - Invalid login
+            /// Second scenario - Invalid data
             driver.get("https://account.blip.ai/login?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fclient_id%3Dblip-portal%26redirect_uri%3Dhttps%253A%252F%252Fportal.blip.ai%252Fauthorize%26response_type%3Did_token%2520token%26scope%3Doffline_access%2520openid%2520profile%2520email%2520api-msging-hub.full_access%26state%3Dac7affc61f2f41b583a38097040b3159%26nonce%3Df3982e609ca24569808c458150071868");
 
+            By emailLocator2 = By.cssSelector("input#email");
+            By pwLocator2 = By.cssSelector("input#password");
+            By loginBtnLocator2 = By.cssSelector("button#blip-login");
 
+            var emailInput2 = driver.findElement(emailLocator2);
+            var pwInput2 = driver.findElement(pwLocator2);
+            var loginButton2 = driver.findElement(loginBtnLocator2);
 
-            System.out.println("done");
+            emailInput2.sendKeys("wbombardellis");
+            pwInput2.sendKeys("123456");
+
+            if (loginButton2.isEnabled())
+                System.out.println("Failed");
+            else
+                System.out.println("Success");
+
         } finally{
             driver.quit();
         }
